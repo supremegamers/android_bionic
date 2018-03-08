f6bd3f52093d978a42a8e75b0afd0b9fb4cc1a0b
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-03-08 12:30:36 +0800

    We need text relocations
    Don't show annoying warnings.

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -3336,7 +3336,6 @@ bool soinfo::link_image(const SymbolLookupList& lookup_list, soinfo* local_group
                      "Enforced-for-API-level-23)", get_realpath());
       return false;
     }
-#endif
     // Make segments writable to allow text relocations to work properly. We will later call
     // phdr_table_protect_segments() after all of them are applied.
     DL_WARN_documented_change(23,
@@ -3344,6 +3343,7 @@ bool soinfo::link_image(const SymbolLookupList& lookup_list, soinfo* local_group
                               "\"%s\" has text relocations",
                               get_realpath());
     add_dlwarning(get_realpath(), "text relocations");
+#endif
     if (phdr_table_unprotect_segments(phdr, phnum, load_bias) < 0) {
       DL_ERR("can't unprotect loadable segments for \"%s\": %s", get_realpath(), strerror(errno));
       return false;
