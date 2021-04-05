de2574c180aaf81571ae72904466fdc5f5382df9
Simão Gomes Viana <devel@superboring.dev>
2021-04-05 15:02:05 +0200

    libc: modify explicit_bzero to match other implementations
    I don't see the result of the function being used anywhere. Although it
    does have an effect on the compilation result, I think that we don't
    need to go as far as returning the pointer here.
    
    Returning a pointer would conflict with the definition in
    external/openssh.
    
    Change-Id: I48b9d3b43356372b10ee65f6c4498acf9542b2d4

--- libc/bionic/explicit_bzero.cpp
+++ libc/bionic/explicit_bzero.cpp
@@ -1,7 +1,7 @@
 #include <string.h>
 
-void* explicit_bzero(void* s, size_t n) {
+void __attribute__((noinline)) explicit_bzero(void* s, size_t n) __attribute__((disable_tail_calls)) {
   void *ptr = memset(s, 0, n);
   __asm__ __volatile__("" : : "r"(ptr) : "memory");
-  return ptr;
+  (void)ptr;
 }
